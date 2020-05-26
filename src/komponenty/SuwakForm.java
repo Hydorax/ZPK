d76af1ed9d2edfca10e8019fd7e97c21d2e8f1e8
Sebastian <Sebastian@Sebastian-PC>
2020-05-26 13:01:22 +0200

    Dodanie property "Orientacja"

--- src/komponenty/SuwakForm.java
+++ src/komponenty/SuwakForm.java
@@ -19,9 +19,12 @@ import java.awt.RenderingHints;
 import java.beans.PropertyChangeListener;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import static javax.swing.SwingConstants.CENTER;
+import static javax.swing.SwingConstants.HORIZONTAL;
+import static javax.swing.SwingConstants.VERTICAL;
 import javax.swing.plaf.basic.BasicSliderUI;
 
 /**
@@ -34,19 +37,26 @@ public class SuwakForm extends JPanel{
     private int wartosc_g;
     private int wartosc_b;
     private Color kolor;
-    private SuwakOrientacja orientacja;
+    private SuwakOrientacja orientacja = SuwakOrientacja.Poziomo;
     private JSlider r;
     private JSlider g;
     private JSlider b;
+    private JLabel label_r;
+    private JLabel label_g;
+    private JLabel label_b;
+    
     
  public SuwakForm(){
      
         
-        this.setLayout(new GridLayout(3, 1));
+        this.setLayout(new GridLayout(3, 2));
         
         JPanel r_panel = new JPanel();
         JPanel g_panel = new JPanel();
         JPanel b_panel = new JPanel();
+        JPanel rr_panel = new JPanel();
+        JPanel gg_panel = new JPanel();
+        JPanel bb_panel = new JPanel();
         
         
         r = new JSlider(0, 255, 0); 
@@ -58,11 +68,18 @@ public class SuwakForm extends JPanel{
              g2d.setColor(Color.blue);
              int tw2 = t.width / 2;
              
-             //g2d.drawLine(t.x, t.y, t.x+t.width, t.y);
-             //g2d.drawLine(t.x, t.y, t.x, t.height);
-             g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
-             g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
-             g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+            
+             
+             if(orientacja == SuwakOrientacja.Pionowo){
+                g2d.drawLine(t.x, t.y, t.x, t.y + t.height);
+                g2d.drawLine(t.x, t.y, t.x + t.width, t.y + (tw2/2));
+                g2d.drawLine(t.x, t.y + t.height, t.x + t.width, t.y + (tw2/2));
+             }else{
+                g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
+                g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
+                g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+             }
+             
              g2d.setColor(Color.red);
              
              wartosc_r = r.getValue();
@@ -85,11 +102,15 @@ public class SuwakForm extends JPanel{
              g2d.setColor(Color.blue);
              int tw2 = t.width / 2;
              
-             //g2d.drawLine(t.x, t.y, t.x+t.width, t.y);
-             //g2d.drawLine(t.x, t.y, t.x, t.height);
-             g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
-             g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
-             g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+             if(orientacja == SuwakOrientacja.Pionowo){
+                g2d.drawLine(t.x, t.y, t.x, t.y + t.height);
+                g2d.drawLine(t.x, t.y, t.x + t.width, t.y + (tw2/2));
+                g2d.drawLine(t.x, t.y + t.height, t.x + t.width, t.y + (tw2/2));
+             }else{
+                g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
+                g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
+                g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+             }
              g2d.setColor(Color.red);
              wartosc_g = g.getValue();
              g_panel.setBackground(new java.awt.Color(0, wartosc_g, 0));
@@ -110,11 +131,15 @@ public class SuwakForm extends JPanel{
              g2d.setColor(Color.blue);
              int tw2 = t.width / 2;
              
-             //g2d.drawLine(t.x, t.y, t.x+t.width, t.y);
-             //g2d.drawLine(t.x, t.y, t.x, t.height);
-             g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
-             g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
-             g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+             if(orientacja == SuwakOrientacja.Pionowo){
+                g2d.drawLine(t.x, t.y, t.x, t.y + t.height);
+                g2d.drawLine(t.x, t.y, t.x + t.width, t.y + (tw2/2));
+                g2d.drawLine(t.x, t.y + t.height, t.x + t.width, t.y + (tw2/2));
+             }else{
+                g2d.drawLine(t.x, t.y, t.x + t.width -1, t.y);
+                g2d.drawLine(t.x, t.y, t.x + tw2, t.y + t.height);
+                g2d.drawLine(t.x + t.width - 1, t.y, t.x + tw2, t.y + t.height);
+             }
              g2d.setColor(Color.red);
              
             wartosc_b = b.getValue(); 
@@ -122,6 +147,8 @@ public class SuwakForm extends JPanel{
             
          }
      });
+        
+        
         b_panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         b_panel.setLayout(new BorderLayout());
         this.add(b_panel);
@@ -130,6 +157,7 @@ public class SuwakForm extends JPanel{
 //     this.add(g);
 //     this.add(b);
      
+
      
  }
     public int getWartosc_r(){
@@ -191,7 +219,20 @@ public class SuwakForm extends JPanel{
     public void setOrientacja(SuwakOrientacja orientacja){
         SuwakOrientacja old_orientacja = this.orientacja;
         this.orientacja = orientacja;
-        firePropertyChange("orientacja",old_orientacja, orientacja);       
+        firePropertyChange("orientacja",old_orientacja, orientacja);   
+         if(orientacja == SuwakOrientacja.Pionowo){
+             setLayout(new GridLayout(1, 3));
+             r.setOrientation(VERTICAL);
+             g.setOrientation(VERTICAL);
+             b.setOrientation(VERTICAL);
+             
+         }else{
+             setLayout(new GridLayout(3, 1));
+             r.setOrientation(HORIZONTAL);
+             g.setOrientation(HORIZONTAL);
+             b.setOrientation(HORIZONTAL);
+         }
+         invalidate();
     }
     
 }
